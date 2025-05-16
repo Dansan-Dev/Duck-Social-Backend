8cac732906eebac19f814d41452ba5d914c0cde4
Daniel Sandstrom <github@danielsandstrom.org>
2025-05-16 09:45:18 +0200

    Implemented ProfileRepository

---
+++ src/main/java/com/example/on_site_react_project_backend/repositories/ProfileRepository.java
@@ -0,0 +1,10 @@
+package com.example.on_site_react_project_backend.repositories;
+
+import com.example.on_site_react_project_backend.models.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, Integer> {
+    Profile findById(int id);
+    Integer deleteById(int id);
+    Integer findTopByOrderByIdDesc();
+}
