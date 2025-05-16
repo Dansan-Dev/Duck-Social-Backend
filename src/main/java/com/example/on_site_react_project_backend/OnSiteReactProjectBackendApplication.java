dfe2ea3ca18e29c8db2211e7cfc01ff868d0d9c5
Daniel Sandstrom <github@danielsandstrom.org>
2025-05-16 09:12:37 +0200

    init commit

---
+++ src/main/java/com/example/on_site_react_project_backend/OnSiteReactProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.on_site_react_project_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnSiteReactProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnSiteReactProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/on_site_react_project_backend/OnSiteReactProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.on_site_react_project_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnSiteReactProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
